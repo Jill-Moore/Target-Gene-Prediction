3a4ece6017619c3c3af3ef1eafa6bb12bb64a058
Jill Moore <Jill.Moore@umassmed.edu>
2019-02-28 09:24:21 -0500

    updated for rrbs and cage

--- Scripts/TargetFinder/process.overlaps.py
+++ Scripts/TargetFinder/process.overlaps.py
@@ -5,23 +5,32 @@ def Create_Element_Dict(elements):
     elementDict={}
     for line in elements:
         line=line.rstrip().split("\t")
-        elementDict[line[3]]=[float(int(line[2])-int(line[1])+1),0]
+        elementDict[line[3]]=[float(int(line[2])-int(line[1])),0]
     return elementDict
 
-def Process_Overlaps(overlaps, elementDict):
+def Process_Overlaps(overlaps, elementDict, mode):
     for line in overlaps:
         line=line.rstrip().split("\t")
-        elementDict[line[3]][1]+=float(line[11])
+        if mode == "peaks":
+            elementDict[line[3]][1]+=float(line[11])
+        elif mode == "cage":
+            elementDict[line[3]][1]+=float(line[9])
+        elif mode == "rrbs":
+            elementDict[line[3]][1]+=float(line[-1])
     for entry in elementDict:
-        print entry+"\t"+str(elementDict[entry][1]/elementDict[entry][0])
+        if elementDict[entry][0] > 0:
+            print entry+"\t"+str(elementDict[entry][1]/elementDict[entry][0])
+        else:
+	    print entry+"\t"+"0"
+
+mode=sys.argv[3]
     
 elements=open(sys.argv[1])
 elementDict=Create_Element_Dict(elements)
 elements.close()
 
-
 overlaps=open(sys.argv[2])
-Process_Overlaps(overlaps,elementDict)
+Process_Overlaps(overlaps,elementDict,mode)
 overlaps.close()
 
 
